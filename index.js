90c98dc1c38359c5d3cfe2e1a3650ed7efee2548
gaurav-08-dev <g8111997@gmail.com>
2023-08-31 16:42:12 +0530

    updates

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ import fetchFromURL from "./fetchModule.js";
 const app = express();
 const port = 4000; // You can change this to any port you prefer
 
-// Define a route
-app.get('/', async (req, res) => {
-    try {
-        const result = await fetchFromURL('https://mathup.com/games/crossbit');
-        console.log(result)
+ // Define a route
+// app.get('/', async (req, res) => {
+//     try {
+//         const result = await fetchFromURL('https://mathup.com/games/crossbit');
+//         console.log(result)
         // console.log(JSON.parse(result))
         // res.send(JSON.parse(result));
-        res.send(result)
+//         res.send(result)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// });
+app.get('/fetch-url/:scrapUrl', async(req, res) => {
+    const urlData = req.params.scrapUrl;
+    // Process the userId and send a response
+    try {
+        const result = await fetchFromURL(urlData);
+        console.log(result)
+        res.send(result);
     } catch (error) {
         console.log(error)
     }
-});
+  });
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
