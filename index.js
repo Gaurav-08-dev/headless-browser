7236fdfa83f1a97a87c46da11ec1fe48843f5fb1
Thanveer Ahamed A <thanveerahamed1225@gmail.com>
2023-08-31 14:54:08 +0530

    push

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const app = express();
 const port = 4000; // You can change this to any port you prefer
 
 // Define a route
-app.get('/', async (req, res) => {
+// app.get('/', async (req, res) => {
+//     res.send('pass the url');
+// });
+
+app.get('/fetch-url/:scrapUrl', async(req, res) => {
+    const urlData = req.params.scrapUrl;
+    // Process the userId and send a response
     try {
-        const result = await fetchFromURL('https://mathup.com/games/crossbit');
+        const result = await fetchFromURL(urlData);
         console.log(result)
         res.send(result);
     } catch (error) {
         console.log(error)
     }
-});
+  });
 
 // Start the server
 app.listen(port, () => {
